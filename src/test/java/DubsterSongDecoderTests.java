086c46a1324e1d2e4798c06bda921bc825a9e537
Samigullin Nikita <nikita.samigullin@travisperkins.co.uk>
2020-11-12 17:56:53 +0200

    Initial commit

---
+++ src/test/java/DubsterSongDecoderTests.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DubsterSongDecoderTests {
+    @Test
+    public void test1() {
+        assertEquals("ABC", Dubster.songDecoder("WUBWUBABCWUB"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals("R L", Dubster.songDecoder("RWUBWUBWUBLWUB"));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals("", Dubster.songDecoder("WUBWUBWUB"));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals("LOREM IPSUM DOLOR SIT AMET", Dubster.songDecoder("LOREMWUBIPSUMWUBWUBWUBWUBWUBDOLORWUBSITWUBWUBAMETWUB"));
+    }
+}
