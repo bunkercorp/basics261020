9f3d6398b1b659bc3003c94419eb667ba59c2e56
Alona Ishchuk <lana07.al@gmail.com>
2021-01-11 02:52:23 +0200

    Add validate method

--- src/main/java/Home8.java
+++ src/main/java/Home8.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -56,6 +58,49 @@ public class Home8 {
                return
                 Stream.of(input.split("[^a-zA-Z0-9'`''-''_']+"))
                 .collect(Collectors.toList()).size();
+    }
+
+    public static boolean validateCode(String input){
+        //Integer.parseInt()
+        //Pattern p = Pattern.compile("[A-Z0-9]{20}//s[0-9]{6}");
+        Pattern pattern = Pattern.compile("[A-Z0-9]{20}\\s[0-9]{6}");
+        Matcher matcher = pattern.matcher(input);
+        if(!matcher.find())
+        return false;
+
+        String right = input.substring(21);
+
+        List<Integer> digit =
+                input.chars()
+                .mapToObj(item -> (int) item - 48)
+                .filter(item -> {
+                    return item >= 0 && item < 10;
+                })
+                .collect(Collectors.toList());
+
+        if( digit.size() != 12)
+            return  false;
+
+        int res =1;
+        Optional <Integer> left = IntStream
+                .range(0, digit.size()/2)
+                //.filter(digit.indexOf() % 2)
+                .mapToObj(elem ->  {
+                    if (elem %2 == 0)
+                        return digit.get(elem) * 10 + digit.get(elem+1);
+                    else
+                       return digit.get(elem);
+                })
+                .collect(Collectors.toList())
+                .stream()
+                .filter(elem -> elem / 10 != 0 )
+                .reduce((l, r) -> l*r);
+
+
+        //System.out.println(left.get());
+        return Long.parseLong(right) == left.get();
+
+
     }
 
 
