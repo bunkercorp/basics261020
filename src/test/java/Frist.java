1c981a1e4a45b0d61795c86c310dd742f71f3eff
archick12 <a.a.piluck@gmail.com>
2021-02-18 21:07:38 +0200

    Added remote web driver call

--- src/test/java/Frist.java
+++ src/test/java/Frist.java
@@ -1,10 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Frist {
 
     @Test
-    public static void test() {
-        System.out.println(System.getProperty("lorem"));
+    public static void test() throws MalformedURLException, InterruptedException {
+        final String whatToGoogle = System.getProperty("query");
+
+        WebDriver browser =
+                new RemoteWebDriver(new URL("http://10.0.0.69:4444/wd/hub"), DesiredCapabilities.chrome());
+
+        browser.get("https://google.com.ua");
+
+        final WebElement inputField = browser.findElement(By.xpath("//div[@id='searchform']//input[@name='q']"));
+        inputField.sendKeys(whatToGoogle);
+
+        final WebElement searchBtn = browser.findElements(By.cssSelector("[id = 'searchform'] [name = 'btnK']")).get(1);
+        browser.findElement(By.cssSelector("[alt = 'Google']")).click();
+        searchBtn.click();
+        Thread.sleep(5000);
+        browser.quit();
+
     }
 
 }
