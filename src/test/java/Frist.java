00a72a900b8454f7f37b6688c9b874ae5ed24f02
archick12 <a.a.piluck@gmail.com>
2021-02-18 21:12:29 +0200

    Added remote web driver call

--- src/test/java/Frist.java
+++ src/test/java/Frist.java
@@ -1,6 +1,4 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -17,14 +15,14 @@ public class Frist {
         WebDriver browser =
                 new RemoteWebDriver(new URL("http://10.0.0.69:4444/wd/hub"), DesiredCapabilities.chrome());
 
-        browser.get("https://google.com.ua");
-
-        final WebElement inputField = browser.findElement(By.xpath("//div[@id='searchform']//input[@name='q']"));
-        inputField.sendKeys(whatToGoogle);
-
-        final WebElement searchBtn = browser.findElements(By.cssSelector("[id = 'searchform'] [name = 'btnK']")).get(1);
-        browser.findElement(By.cssSelector("[alt = 'Google']")).click();
-        searchBtn.click();
+        browser.get(String.format("https://%s", whatToGoogle));
+//
+//        final WebElement inputField = browser.findElement(By.xpath("//div[@id='searchform']//input[@name='q']"));
+//        inputField.sendKeys(whatToGoogle);
+//
+//        final WebElement searchBtn = browser.findElements(By.cssSelector("[id = 'searchform'] [name = 'btnK']")).get(1);
+//        browser.findElement(By.cssSelector("[alt = 'Google']")).click();
+//        searchBtn.click();
         Thread.sleep(5000);
         browser.quit();
 
