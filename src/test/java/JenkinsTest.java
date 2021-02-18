bff4a2fe42a671dab40952ce169d68531cf95a4b
OlgaVovkVovk <olga.vovk.oiv@gmail.com>
2021-02-18 20:02:07 +0200

    Update6

--- src/test/java/JenkinsTest.java
+++ src/test/java/JenkinsTest.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 
 public class JenkinsTest {
     @Test
-    public static void test() {
+    public void test() {
         System.out.println(System.getProperty("Olala"));
     }
 }
