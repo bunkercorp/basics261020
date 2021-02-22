a7e89165e9f2d4076da4183b2de7a91f64707327
OlgaVovkVovk <olga.vovk.oiv@gmail.com>
2021-02-22 21:09:54 +0200

    new commit3

--- src/test/java/testSuites/TestOne.java
+++ src/test/java/testSuites/TestOne.java
@@ -7,15 +7,13 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 
-import static testSuites.Browser.getBrowser;
-
 public class TestOne {
     @Test
     public static void test() throws MalformedURLException {
 
-        getBrowser().get("https://www.native-english.ru/grammar");
+        Browser.getBrowser().get("https://www.native-english.ru/grammar");
 
-        final WebElement titleOfArticle = getBrowser().findElements(By.cssSelector("[class = 'content'] [class = 'page'] [class = 'page__content'] [class = 'title']")).get(0);
+        final WebElement titleOfArticle = Browser.getBrowser().findElements(By.cssSelector("[class = 'content'] [class = 'page'] [class = 'page__content'] [class = 'title']")).get(0);
         String title = titleOfArticle.getText();
 
         Assert.assertEquals(title, "Грамматика английского языка");
--- src/test/java/testSuites/TestTwo.java
+++ src/test/java/testSuites/TestTwo.java
@@ -7,15 +7,13 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 
-import static testSuites.Browser.getBrowser;
-
 public class TestTwo {
     @Test
     public static void test() throws MalformedURLException {
 
-        getBrowser().get("https://ru.wikipedia.org/wiki/PandaBoard");
+        Browser.getBrowser().get("https://ru.wikipedia.org/wiki/PandaBoard");
 
-        final WebElement titleOfArticle = getBrowser().findElements(By.cssSelector("[id = 'content'] [id = 'firstHeading']")).get(0);
+        final WebElement titleOfArticle = Browser.getBrowser().findElements(By.cssSelector("[id = 'content'] [id = 'firstHeading']")).get(0);
         String title = titleOfArticle.getText();
 
         Assert.assertEquals(title, "PandaBoard");
