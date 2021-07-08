f124ea73c93fe58e6df24874c5911e8b215e620b
archick12 <a.a.piluck@gmail.com>
2021-07-08 20:06:26 +0300

    Working hard

--- src/test/java/Victim.java
+++ src/test/java/Victim.java
@@ -3,8 +3,10 @@ import org.testng.annotations.Test;
 public class Victim {
 
     @Test
-    public static void test(){
-        System.out.println("Worked!!!!");
+    public static void test() {
+        final String lorem = System.getProperty("lorem");
+        final int ipsum = Integer.parseInt(System.getProperty("ipsum"));
+        System.out.printf("lorem='%s' ipsum=%d", lorem, ipsum);
     }
 
 }
