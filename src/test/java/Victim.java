9281b9b25f6ecac55711ee7e9255c06d066c6cf4
archick12 <a.a.piluck@gmail.com>
2021-07-12 20:55:51 +0300

    Added file operation

--- src/test/java/Victim.java
+++ src/test/java/Victim.java
@@ -1,17 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 
 public class Victim {
 
     @Test
     public static void test() throws IOException {
-        final String payload = System.getProperty("payload");
-        BufferedWriter writer = new BufferedWriter(new FileWriter("payload.txt"));
-        writer.write(payload);
-        writer.close();
+        final String url = System.getProperty("payload");
+        DesiredCapabilities caps = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL("http://10.0.0.69:4444/wd/hub"), caps);
+        driver.get(url);
     }
 
 }
